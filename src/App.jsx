73829bcb4972fea0cfa8dcdd9796f315d6737e9d
Your Name <your-email@gmail.com>
2026-01-21 16:44:57 +0530

    Temp rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Main from "./components/Main/Main";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/sidebar_temp/Sidebar";
 
 const App = () => {
   const [chats, setChats] = useState([]);
